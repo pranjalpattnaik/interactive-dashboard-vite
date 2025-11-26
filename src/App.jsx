5f746f8e971c08808b571f2b9812bd71da431e0e
Pranjal Pattnaik <pranjalpattnaik@Pranjals-MacBook-Air.local>
2025-11-27 01:44:06 +0530

    Final fix: Add all missing files and config

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import InteractiveMethodologyDashboard from './InteractiveMethodologyDashboard';
+
+function App() {
+  return (
+    <InteractiveMethodologyDashboard />
+  );
+}
+
+export default App;
